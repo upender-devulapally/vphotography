73d9c277fe5f775dde1fde3f9ec9a0327f81f7c3
Upender <upender.devulapally@gmail.com>
2013-07-10 19:52:27 +0530

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vphotography::Application.config.secret_token = '33df858ba2c84375d3526c6bda8925cc1be6ed04bdc281c4dcec10d9d915f652cac2202ea0f2b2d4542c241c6e6386b4c08bee66ba91078f131e202bb511f864'
